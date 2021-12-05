2cbd8c45eb39a7e628d41e58c23f7b2c6b7d1702
said <saiddemirhan@gmail.com>
2021-12-06 00:41:37 +0100

    v2

--- python.py
+++ python.py
@@ -25,13 +25,24 @@ print(len(soyisim))# sayisim kac karakterse onu gösterir.
 
 print(soyisim[2]) #(n)bir stringin istenilen degerine ulasir
 
+print(soyisim[-1]) #(n) string deki son elemani getirir.
+
+print(soyisim[len(soyisim)-1]) #(n) string deki son elemani getirir.
+
+print(soyisim[-len(soyisim)]) #(c) string deki ilk elemani getirir.
+
 print(soyisim[0:2]) #(ca) bir stringdeki istenilen indexteki karakterleri getirir.
 
 print(soyisim[1:]) #(an) belirtilen index ve sonrasini getirir.
+print(soyisim[-3:]) #(an) belirtilen index ve sonrasini getirir.
 
 print(soyisim[:2]) #(ca) belirtilen indexin öncesini getirir.
+print(soyisim[:-1]) #(ca) belirtilen indexin öncesini getirir.
 
-print(soyisim[-3:-1]) #(ca) geriden baslayarak da istenilen karakterler yazdirirlabilir.
+print(soyisim[-3:-1]) #(ca) geriden baslayarak da istenilen karakterler yazdirilabilir.
+
+print(soyisim[::2]) #(cn) bir karakter atlayarak bir string'i getirir.
+
 
 
 
