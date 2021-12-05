76f26941d09cf46b936f86635ff2f540bcb3c550
said <saiddemirhan@gmail.com>
2021-12-05 23:45:42 +0100

    first

---
+++ python.py
@@ -0,0 +1,23 @@
+print("Hosgeldiniz!")
+isim="ali"
+soyisim="can"
+print("Ad: ",isim,"\nSoyad: ",soyisim)
+
+rakamalar=[1,2,3,4]
+print("rakamlar",rakamalar)
+print(type(rakamalar))#<class 'list'>
+
+esyalar=("kalem","masa","kitap")
+print("esyalar",esyalar)
+print(type(esyalar))#<class 'tuple'>
+
+harfler={"a","b","c"}
+print("harfler",harfler)
+print(type(harfler))#<class 'set'>
+
+notlar={"Tukce":5,"Almanca":4,"Ingilizce":"muaf"}
+print(notlar)
+print(type(notlar))#<class 'dict'>
+
+print(len(soyisim))
+
